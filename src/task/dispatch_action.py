22444329f32a0f0b98e5aa11b21f4f23c1f813b2
Eberte Sampaio <deveberte@gmail.com>
2026-06-04 17:34:03 -0300

    refactor: remove unnecessary dispatch_action.py

--- src/task/dispatch_action.py
+++
@@ -1,9 +0,0 @@
-import argparse
-from argparse import ArgumentParser, Namespace
-
-
-class DispatchAction(argparse.Action):
-
-    def __call__(self, parser: ArgumentParser, namespace: Namespace, values, option_strings= None):
-        setattr(namespace, 'command_allowed', self.dest)
-        setattr(namespace, 'command_value', values)
